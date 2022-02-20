42c5c8241b5bf3039b4ce38535577a1039337314
Fowler Walker <fsw0006@auburn.edu>
2022-02-19 19:07:08 -0600

    unfunctioning ui with no file input

--- src/status_report_gui/Base_UI.java
+++ src/status_report_gui/Base_UI.java
@@ -1,6 +1,7 @@
 import javax.swing.event.*;
 import javax.swing.*;
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.*;
 
 public class Base_UI {
@@ -13,33 +14,57 @@ public class Base_UI {
       //test text area for print parms
       JTextArea out = new JTextArea();
       
-      JPanel text_panel = new JPanel(); // the panel is not visible in output
+      JPanel parms_panel = new JPanel(); // the panel is not visible in output
+      parms_panel.setLayout(new GridLayout(0, 1));
       
+      JPanel n_temp_panel = new JPanel(); // the panel is not visible in output
       JLabel n_t_label = new JLabel("Nozzle Temperature");
       JTextField n_t_tf = new JTextField(20);
       
+      JPanel s_temp_panel = new JPanel(); // the panel is not visible in output
       JLabel s_t_label = new JLabel("Print Surface Temperature");
       JTextField s_t_tf = new JTextField(20);
       
+      JPanel p_speed_panel = new JPanel(); // the panel is not visible in output
       JLabel p_s_label = new JLabel("Print Speed");
       JTextField p_s_tf = new JTextField(20);
       
-      JButton submit = new JButton("Submit");
+      JPanel i_pattern_panel = new JPanel(); // the panel is not visible in output
+      JLabel i_p_label = new JLabel("Infill Pattern");
+      String[] i_p_options = {"Cross Lattice", "Square Lattice"};
+      JComboBox<String> i_p_cb = new JComboBox<String>(i_p_options);
+      
+      JPanel i_density_panel = new JPanel(); // the panel is not visible in output
+      JLabel i_d_label = new JLabel("Infill Density");
+      JTextField i_d_tf = new JTextField(20);
+      
+      JButton submit = new JButton("Ready Print");
       
       // Components Added using Flow Layout
-      text_panel.add(n_t_label); 
-      text_panel.add(n_t_tf);
+      n_temp_panel.add(n_t_label); 
+      n_temp_panel.add(n_t_tf);
       
-      text_panel.add(s_t_label); 
-      text_panel.add(s_t_tf);
+      s_temp_panel.add(s_t_label); 
+      s_temp_panel.add(s_t_tf);
       
-      text_panel.add(p_s_label); 
-      text_panel.add(p_s_tf);
+      p_speed_panel.add(p_s_label); 
+      p_speed_panel.add(p_s_tf);
       
-      text_panel.add(submit);
+      i_pattern_panel.add(i_p_label); 
+      i_pattern_panel.add(i_p_cb);
+      
+      i_density_panel.add(i_d_label); 
+      i_density_panel.add(i_d_tf);
+      
+      parms_panel.add(n_temp_panel);
+      parms_panel.add(s_temp_panel);
+      parms_panel.add(p_speed_panel);
+      parms_panel.add(i_pattern_panel);
+      parms_panel.add(i_density_panel);
+      parms_panel.add(submit);
       
       //Adding Components to the frame.
-      frame.getContentPane().add(BorderLayout.WEST, text_panel);
+      frame.getContentPane().add(BorderLayout.WEST, parms_panel);
       frame.getContentPane().add(BorderLayout.EAST, out);
       frame.setVisible(true);
    }
