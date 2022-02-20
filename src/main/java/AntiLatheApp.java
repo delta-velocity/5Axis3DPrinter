adb2ff24608282184dfd244e86a4d1e84741670e
sea0416 <sea0416@naver.com>
2022-02-19 19:25:46 -0600

    Made ImportFile to import .stl file
    Prototype of import .stl file
    Made open and save.

--- src/main/java/AntiLatheApp.java
+++ src/main/java/AntiLatheApp.java
@@ -47,122 +47,123 @@ import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
 
 public class AntiLatheApp {
-	private GLFWErrorCallback errorCallback;
-	private GLFWKeyCallback keyCallback;
+   private GLFWErrorCallback errorCallback;
+   private GLFWKeyCallback keyCallback;
 
-	private long window;
+   private long window;
 
-	private float sp = 0.0f;
-	private boolean swapcolor = false;
+   private float sp = 0.0f;
+   private boolean swapcolor = false;
 
-	private void init() {
-		glfwSetErrorCallback(errorCallback = GLFWErrorCallback.createPrint(System.err));
+   private void init() {
+      glfwSetErrorCallback(errorCallback = GLFWErrorCallback.createPrint(System.err));
+   
+      if (!glfwInit())
+         throw new IllegalStateException("Unable to initialize GLFW");
+   
+      glfwDefaultWindowHints();
+      glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+      glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+   
+      int WIDTH = 300;
+      int HEIGHT = 300;
+   
+      window = glfwCreateWindow(WIDTH, HEIGHT, "Hello LWJGL3", NULL, NULL);
+      if (window == NULL)
+         throw new RuntimeException("Failed to create the GLFW window");
+   
+      glfwSetKeyCallback(window, keyCallback = 
+         new GLFWKeyCallback() {
+            @Override
+            public void invoke(long window, int key, int scancode, int action, int mods) {
+               if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+                  glfwSetWindowShouldClose(window, true);
+            }
+         });
+   
+      GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+      glfwSetWindowPos(window, (vidmode.width() - WIDTH) / 2, (vidmode.height() - HEIGHT) / 2);
+   
+      glfwMakeContextCurrent(window);
+      glfwSwapInterval(1);
+   
+      glfwShowWindow(window);
+   }
 
-		if (!glfwInit())
-			throw new IllegalStateException("Unable to initialize GLFW");
+   private void update() {
+      sp = sp + 0.001f;
+      if (sp > 1.0f) {
+         sp = 0.0f;
+         swapcolor = !swapcolor;
+      }
+   }
 
-		glfwDefaultWindowHints();
-		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+   private void render() {
+      drawQuad();
+   }
 
-		int WIDTH = 300;
-		int HEIGHT = 300;
+   private void drawQuad() {
+      if (!swapcolor) {
+         glColor3f(0.0f, 1.0f, 0.0f);
+      } else {
+         glColor3f(0.0f, 0.0f, 1.0f);
+      }
+   
+      glBegin(GL_QUADS);
+      {
+      
+         glVertex3f(-sp, -sp, 0.0f);
+         glVertex3f(sp, -sp, 0.0f);
+         glVertex3f(sp, sp, 0.0f);
+         glVertex3f(-sp, sp, 0.0f);
+      }
+      glEnd();
+   
+   }
 
-		window = glfwCreateWindow(WIDTH, HEIGHT, "Hello LWJGL3", NULL, NULL);
-		if (window == NULL)
-			throw new RuntimeException("Failed to create the GLFW window");
+   private void loop() {
+      GL.createCapabilities();
+      System.out.println("----------------------------");
+      System.out.println("OpenGL Version : " + glGetString(GL_VERSION));
+      System.out.println("OpenGL Max Texture Size : " + glGetInteger(GL_MAX_TEXTURE_SIZE));
+      System.out.println("OpenGL Vendor : " + glGetString(GL_VENDOR));
+      System.out.println("OpenGL Renderer : " + glGetString(GL_RENDERER));
+      System.out.println("OpenGL Extensions supported by your card : ");
+      String extensions = glGetString(GL_EXTENSIONS);
+      String[] extArr = extensions.split("\\ ");
+      for (int i = 0; i < extArr.length; i++) {
+         System.out.println(extArr[i]);
+      }
+      System.out.println("----------------------------");
+   
+      while (!glfwWindowShouldClose(window)) {
+         if (!swapcolor) {
+            glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+         } else {
+            glClearColor(0.0f, 1.0f, 0.0f, 0.0f);
+         }
+      
+         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+         update();
+         render();
+         glfwSwapBuffers(window);
+      
+         glfwPollEvents();
+      }
+   }
+   public void run() {
+      System.out.println("Hello LWJGL3 " + Version.getVersion() + "!");
+   
+      try {
+         init();
+         loop();
+         glfwDestroyWindow(window);
+      } finally {
+         glfwTerminate();
+      }
+   }
 
-		glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
-			@Override
-			public void invoke(long window, int key, int scancode, int action, int mods) {
-				if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-					glfwSetWindowShouldClose(window, true);
-			}
-		});
-
-		GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-		glfwSetWindowPos(window, (vidmode.width() - WIDTH) / 2, (vidmode.height() - HEIGHT) / 2);
-
-		glfwMakeContextCurrent(window);
-		glfwSwapInterval(1);
-
-		glfwShowWindow(window);
-	}
-
-	private void update() {
-		sp = sp + 0.001f;
-		if (sp > 1.0f) {
-			sp = 0.0f;
-			swapcolor = !swapcolor;
-		}
-	}
-
-	private void render() {
-		drawQuad();
-	}
-
-	private void drawQuad() {
-		if (!swapcolor) {
-			glColor3f(0.0f, 1.0f, 0.0f);
-		} else {
-			glColor3f(0.0f, 0.0f, 1.0f);
-		}
-
-		glBegin(GL_QUADS);
-		{
-
-			glVertex3f(-sp, -sp, 0.0f);
-			glVertex3f(sp, -sp, 0.0f);
-			glVertex3f(sp, sp, 0.0f);
-			glVertex3f(-sp, sp, 0.0f);
-		}
-		glEnd();
-
-	}
-
-	private void loop() {
-		GL.createCapabilities();
-		System.out.println("----------------------------");
-		System.out.println("OpenGL Version : " + glGetString(GL_VERSION));
-		System.out.println("OpenGL Max Texture Size : " + glGetInteger(GL_MAX_TEXTURE_SIZE));
-		System.out.println("OpenGL Vendor : " + glGetString(GL_VENDOR));
-		System.out.println("OpenGL Renderer : " + glGetString(GL_RENDERER));
-		System.out.println("OpenGL Extensions supported by your card : ");
-		String extensions = glGetString(GL_EXTENSIONS);
-		String[] extArr = extensions.split("\\ ");
-		for (int i = 0; i < extArr.length; i++) {
-			System.out.println(extArr[i]);
-		}
-		System.out.println("----------------------------");
-
-		while (!glfwWindowShouldClose(window)) {
-			if (!swapcolor) {
-				glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
-			} else {
-				glClearColor(0.0f, 1.0f, 0.0f, 0.0f);
-			}
-
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-			update();
-			render();
-			glfwSwapBuffers(window);
-
-			glfwPollEvents();
-		}
-	}
-	public void run() {
-		System.out.println("Hello LWJGL3 " + Version.getVersion() + "!");
-
-		try {
-			init();
-			loop();
-			glfwDestroyWindow(window);
-		} finally {
-			glfwTerminate();
-		}
-	}
-
-	public static void main(String[] args) {
-		new AntiLatheApp().run();
-	}
+   public static void main(String[] args) {
+      new AntiLatheApp().run();
+   }
 }
