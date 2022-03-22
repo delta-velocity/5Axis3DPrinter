fc7cd21e1fb062a98408f0974992cf3707473d84
sea0416 <sea0416@naver.com>
2022-03-21 23:44:39 -0500

    Made project file

--- src/main/java/AntiLatheApp.java
+++ src/main/java/AntiLatheApp.java
@@ -15,110 +15,111 @@ import static org.lwjgl.system.MemoryUtil.*;
 public class AntiLatheApp {
 
 	// The window handle
-	private long window;
+   private long window;
 
-	private int defaultWidth = 1440;
-	private int defaultHeight = 720;
-	private boolean toggle = false;
+   private int defaultWidth = 1440;
+   private int defaultHeight = 720;
+   private boolean toggle = false;
 
-	private static STL stl;
+   private static STL stl;
 
-	public static void main(String[] args) throws Exception {
-		stl = new STL("C:\\Users\\Delta\\Documents\\5Axis3DPrinter\\GoPro_-_Gerade.stl");
-		AntiLatheApp app = new AntiLatheApp();
-		app.run();
-	}
-	public void run() {
-		System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+   public static void main(String[] args) throws Exception {
+      stl = new STL("C:\\Users\\Delta\\Documents\\5Axis3DPrinter\\GoPro_-_Gerade.stl");
+      AntiLatheApp app = new AntiLatheApp();
+      app.run();
+   }
+   public void run() {
+      System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+   
+      init();
+      loop();
+   
+   	// Free the window callbacks and destroy the window
+      glfwFreeCallbacks(window);
+      glfwDestroyWindow(window);
+   
+   	// Terminate GLFW and free the error callback
+      glfwTerminate();
+      glfwSetErrorCallback(null).free();
+   }
 
-		init();
-		loop();
+   private void init() {
+   	// Setup an error callback. The default implementation
+   	// will print the error message in System.err.
+      GLFWErrorCallback.createPrint(System.err).set();
+   
+   	// Initialize GLFW. Most GLFW functions will not work before doing this.
+      if ( !glfwInit() )
+         throw new IllegalStateException("Unable to initialize GLFW");
+   
+   	// Configure GLFW
+      glfwDefaultWindowHints(); // optional, the current window hints are already the default
+      glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+      glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+   
+   	// Create the window
+      window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+      if ( window == NULL )
+         throw new RuntimeException("Failed to create the GLFW window");
+   
+   	// Setup a key callback. It will be called every time a key is pressed, repeated or released.
+      glfwSetKeyCallback(window, 
+         (window, key, scancode, action, mods) -> {
+            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+               glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+         });
+   
+   	// Get the thread stack and push a new frame
+      try ( MemoryStack stack = stackPush() ) {
+         IntBuffer pWidth = stack.mallocInt(1); // int*
+         IntBuffer pHeight = stack.mallocInt(1); // int*
+      
+      	// Get the window size passed to glfwCreateWindow
+         glfwGetWindowSize(window, pWidth, pHeight);
+      
+      	// Get the resolution of the primary monitor
+         GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+      
+      	// Center the window
+         glfwSetWindowPos(
+            	window,
+            	(vidmode.width() - pWidth.get(0)) / 2,
+            	(vidmode.height() - pHeight.get(0)) / 2
+            );
+      } // the stack frame is popped automatically
+   
+   	// Make the OpenGL context current
+      glfwMakeContextCurrent(window);
+   	// Enable v-sync
+      glfwSwapInterval(1);
+   
+   	// Make the window visible
+      glfwShowWindow(window);
+   }
 
-		// Free the window callbacks and destroy the window
-		glfwFreeCallbacks(window);
-		glfwDestroyWindow(window);
-
-		// Terminate GLFW and free the error callback
-		glfwTerminate();
-		glfwSetErrorCallback(null).free();
-	}
-
-	private void init() {
-		// Setup an error callback. The default implementation
-		// will print the error message in System.err.
-		GLFWErrorCallback.createPrint(System.err).set();
-
-		// Initialize GLFW. Most GLFW functions will not work before doing this.
-		if ( !glfwInit() )
-			throw new IllegalStateException("Unable to initialize GLFW");
-
-		// Configure GLFW
-		glfwDefaultWindowHints(); // optional, the current window hints are already the default
-		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-
-		// Create the window
-		window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
-		if ( window == NULL )
-			throw new RuntimeException("Failed to create the GLFW window");
-
-		// Setup a key callback. It will be called every time a key is pressed, repeated or released.
-		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-			if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-				glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
-		});
-
-		// Get the thread stack and push a new frame
-		try ( MemoryStack stack = stackPush() ) {
-			IntBuffer pWidth = stack.mallocInt(1); // int*
-			IntBuffer pHeight = stack.mallocInt(1); // int*
-
-			// Get the window size passed to glfwCreateWindow
-			glfwGetWindowSize(window, pWidth, pHeight);
-
-			// Get the resolution of the primary monitor
-			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-			// Center the window
-			glfwSetWindowPos(
-					window,
-					(vidmode.width() - pWidth.get(0)) / 2,
-					(vidmode.height() - pHeight.get(0)) / 2
-			);
-		} // the stack frame is popped automatically
-
-		// Make the OpenGL context current
-		glfwMakeContextCurrent(window);
-		// Enable v-sync
-		glfwSwapInterval(1);
-
-		// Make the window visible
-		glfwShowWindow(window);
-	}
-
-	private void loop() {
-		// This line is critical for LWJGL's interoperation with GLFW's
-		// OpenGL context, or any context that is managed externally.
-		// LWJGL detects the context that is current in the current thread,
-		// creates the GLCapabilities instance and makes the OpenGL
-		// bindings available for use.
-		GL.createCapabilities();
-
-		// Set the clear color
-		glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
-
-		// Run the rendering loop until the user has attempted to close
-		// the window or has pressed the ESCAPE key.
-		while ( !glfwWindowShouldClose(window) ) {
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
-
-			glfwSwapBuffers(window); // swap the color buffers
-
-			// Poll for window events. The key callback above will only be
-			// invoked during this call.
-			glfwPollEvents();
-		}
-	}
+   private void loop() {
+   	// This line is critical for LWJGL's interoperation with GLFW's
+   	// OpenGL context, or any context that is managed externally.
+   	// LWJGL detects the context that is current in the current thread,
+   	// creates the GLCapabilities instance and makes the OpenGL
+   	// bindings available for use.
+      GL.createCapabilities();
+   
+   	// Set the clear color
+      glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+   
+   	// Run the rendering loop until the user has attempted to close
+   	// the window or has pressed the ESCAPE key.
+      while ( !glfwWindowShouldClose(window) ) {
+         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+      
+         glfwSwapBuffers(window); // swap the color buffers
+      
+      	// Poll for window events. The key callback above will only be
+      	// invoked during this call.
+         glfwPollEvents();
+      }
+   }
 
 
 
--- src/main/java/ImportFile.java
+++ src/main/java/ImportFile.java
@@ -5,7 +5,11 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.io.*;
 
 
 
@@ -13,31 +17,32 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 public class ImportFile extends JFrame implements ActionListener
 {
    private JFileChooser jfc = new JFileChooser();
-   private JButton jbt_open = new JButton("Open");
    private JButton jbt_save = new JButton("Save");
+   private JButton jbt_project = new JButton("Create a New Project");
    private JLabel jlb = new JLabel(" "); 
+  
           
    public ImportFile()
    {
       super("Open");
       this.init();
       this.start();
-      this.setSize(400,200);
+      this.setSize(400,100);
       this.setVisible(true);
    }
    
    public void init()
    {
       getContentPane().setLayout(new FlowLayout());
-      add(jbt_open);
       add(jbt_save);
+      add(jbt_project);
       add(jlb);
    }
    
    public void start()
    {
-      jbt_open.addActionListener(this);
       jbt_save.addActionListener(this);
+      jbt_project.addActionListener(this);
       jfc.setFileFilter(new FileNameExtensionFilter("txt", "txt"));
       jfc.setMultiSelectionEnabled(false);
    }
@@ -45,18 +50,46 @@ public class ImportFile extends JFrame implements ActionListener
    @Override
        public void actionPerformed(ActionEvent arg0)
    {
-      if(arg0.getSource() == jbt_open)
-      {
-         if(jfc.showOpenDialog(this) == JFileChooser.APPROVE_OPTION)
-         {
-            jlb.setText("Open Path : " + jfc.getSelectedFile().toString());
-         }
-      }
-      else if(arg0.getSource() == jbt_save)
+     // It updates the saved project file.
+      if(arg0.getSource() == jbt_save)
       {
          if(jfc.showSaveDialog(this) == JFileChooser.APPROVE_OPTION)
          {
             jlb.setText("Save Path : " + jfc.getSelectedFile().toString() + "." + jfc.getFileFilter().getDescription());
+            String path = jfc.getSelectedFile().toString() + "." + jfc.getFileFilter().getDescription();
+            jlb.setText("Project File Path :" + path);
+            
+            try{
+               PrintWriter fw = new PrintWriter(path);
+               String data = "";
+               fw.println(data);
+               fw.close();
+            }
+            catch (Exception exp){
+               exp.printStackTrace();
+               JOptionPane.showMessageDialog(this, "Failed to create profile", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+         }
+      }
+      
+      else if(arg0.getSource() == jbt_project)
+      {
+         if(jfc.showSaveDialog(this) == JFileChooser.APPROVE_OPTION)
+         {
+            String path = jfc.getSelectedFile().toString() + "." + jfc.getFileFilter().getDescription();
+            jlb.setText("Project File Path :" + path);
+            
+            try{
+               PrintWriter fw = new PrintWriter(path);
+               String data = "";
+               fw.println(data);
+               fw.close();
+            }
+            catch (Exception exp){
+               exp.printStackTrace();
+               JOptionPane.showMessageDialog(this, "Failed to create profile", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+            
          }
       }
    }
