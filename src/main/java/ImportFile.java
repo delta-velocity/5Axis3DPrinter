708e31589bf31826629aa713361957c0dcb11c0c
sea0416 <sea0416@naver.com>
2022-04-27 13:40:08 -0500

    Prj file

--- src/main/java/ImportFile.java
+++ src/main/java/ImportFile.java
@@ -10,6 +10,9 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
 import java.io.*;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.io.IOException;
 
 
 
@@ -17,7 +20,6 @@ import java.io.*;
 public class ImportFile extends JFrame implements ActionListener
 {
    private JFileChooser jfc = new JFileChooser();
-   private JButton jbt_save = new JButton("Save");
    private JButton jbt_project = new JButton("Create a New Project");
    private JLabel jlb = new JLabel(" "); 
   
@@ -34,64 +36,83 @@ public class ImportFile extends JFrame implements ActionListener
    public void init()
    {
       getContentPane().setLayout(new FlowLayout());
-      add(jbt_save);
       add(jbt_project);
       add(jlb);
    }
    
    public void start()
    {
-      jbt_save.addActionListener(this);
       jbt_project.addActionListener(this);
       jfc.setFileFilter(new FileNameExtensionFilter("txt", "txt"));
       jfc.setMultiSelectionEnabled(false);
    }
-        
+   
+   
+   public void configure(HashMap<String, String> inputs, String path)
+   {
+      try{
+         PrintWriter fw = new PrintWriter(path);
+         String theText = "";
+         for (String label_text : inputs.keySet()) 
+         {
+            String choice_text = inputs.get(label_text);   
+            theText += label_text + "\n" + choice_text + "\n";
+         }
+         fw.println(theText);
+         fw.close();
+      }
+      catch (Exception exp){
+         exp.printStackTrace();
+         
+      }
+   }
+   
+   public HashMap<String, String> load(String path) throws IOException
+   {
+      Scanner scanner = new Scanner(new File(path));
+      int i = 0;
+      String label = "";
+      String value = "";
+      HashMap<String, String> inputs = new HashMap<String, String>();
+      while (scanner.hasNext()) 
+      {  
+         if (i % 2 == 0)
+         {
+            label = scanner.next();
+         }
+         else
+         {
+            value = scanner.next();
+            inputs.put(label, value);
+         }
+      }
+      return inputs;
+   }
+   
+  
    @Override
        public void actionPerformed(ActionEvent arg0)
    {
-     // It updates the saved project file.
-      if(arg0.getSource() == jbt_save)
+     // It saves a project file.
+              
+      if(arg0.getSource() == jbt_project)
       {
          if(jfc.showSaveDialog(this) == JFileChooser.APPROVE_OPTION)
          {
-            jlb.setText("Save Path : " + jfc.getSelectedFile().toString() + "." + jfc.getFileFilter().getDescription());
             String path = jfc.getSelectedFile().toString() + "." + jfc.getFileFilter().getDescription();
             jlb.setText("Project File Path :" + path);
-            
-            try{
-               PrintWriter fw = new PrintWriter(path);
-               String data = "";
-               fw.println(data);
-               fw.close();
-            }
-            catch (Exception exp){
-               exp.printStackTrace();
-               JOptionPane.showMessageDialog(this, "Failed to create profile", "Error", JOptionPane.ERROR_MESSAGE);
-            }
+            //This is a test HashMap
+            HashMap<String, String> inputs = new HashMap<String, String>();
+            inputs.put("Song about Christmas is Chrismas Song", "Folwer Scott Walker agrees"); 
+            inputs.put("Nice", "Finally");
+            this.configure(inputs, path);
+                           
          }
+         
       }
       
-      else if(arg0.getSource() == jbt_project)
-      {
-         if(jfc.showSaveDialog(this) == JFileChooser.APPROVE_OPTION)
-         {
-            String path = jfc.getSelectedFile().toString() + "." + jfc.getFileFilter().getDescription();
-            jlb.setText("Project File Path :" + path);
-            
-            try{
-               PrintWriter fw = new PrintWriter(path);
-               String data = "";
-               fw.println(data);
-               fw.close();
-            }
-            catch (Exception exp){
-               exp.printStackTrace();
-               JOptionPane.showMessageDialog(this, "Failed to create profile", "Error", JOptionPane.ERROR_MESSAGE);
-            }
-            
-         }
-      }
+      // It loads the saved project file.
    }
+   
        
 }
